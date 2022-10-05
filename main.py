3304154e380b7956dcaadc9666a0c491775f6942
junhyeon Jo <colorful105@cu.ac.kr>
2022-10-06 00:17:05 +0900

    Add camera Fix function and scrolling function
    By disabling the camera's interaction, the screen is frozen and the scroll function is implemented only in the frozen state using the return value of the interaction.

--- main.py
+++ main.py
@@ -10,15 +10,17 @@ class MyApp(QWidget):
         super().__init__()
         self.initUI()
 
+
     def initUI(self):
         global canvas, view, grid
-
+        self.flag = 1
         canvas = vispy.scene.SceneCanvas()
         view = canvas.central_widget.add_view()
         grid = visuals.GridLines(parent=view.scene)
 
         view.add(grid)
         view.camera = 'turntable'
+        self.roll = view.camera.roll
 
         button = QPushButton('Button')
         button.clicked.connect(self.openfile)
@@ -36,25 +38,33 @@ class MyApp(QWidget):
         canvas.events.key_press.connect(self.on_button_press)
         canvas.events.mouse_move.connect(self.on_mouse_move)
         canvas.events.mouse_press.connect(self.on_mouse_press)
-
+        canvas.events.mouse_move.disconnect(self.on_mouse_press)
     def openfile(self):
         QFileDialog.getOpenFileName(self, 'Open file', './')
 
     def on_button_press(self, event):
-
-        if event.key == 'c':  # create object
+        if event.key == 'c': # create object
             self.object = self.EditVisual()
             view.camera.distance = abs(self.data).max() * 2.5
             self.meshes.append([self.data])
             self.update()
 
+        if event.key == 'f': # camera scene fix
+            view.camera.interactive = not view.camera.interactive
+
+            if view.camera.interactive == 0:
+                canvas.events.mouse_move.connect(self.on_mouse_press)
+            else:
+                canvas.events.mouse_move.disconnect(self.on_mouse_press)
+
     def EditVisual(self):
         global ids, kwargs, colors
-        self.data = np.random.randn(30, 3)
+        self.size = 30
+        self.data = np.random.randn(100, 3)
         cmap = color.get_colormap('viridis')
         colors = np.concatenate(list(map(cmap.map, np.linspace(0, 1, len(self.data)))))
         self.sphere = visuals.Markers(parent=view.scene)
-        kwargs = dict(symbol='o', size=45, edge_color=None)
+        kwargs = dict(symbol='o', size=self.size, edge_width = 0, edge_color=None)
         ids = np.arange(1, len(self.data) + 1, dtype=np.uint32).view(np.uint8)
         ids = ids.reshape(-1, 4)
         ids = np.divide(ids, 255, dtype=np.float32)
@@ -62,36 +72,41 @@ class MyApp(QWidget):
 
         return self.sphere
 
-    def on_mouse_press(self, event):
-        click_radius = 22.5
-        if event.button == 1:
-            pos = canvas.transforms.canvas_transform.map(event.pos)
-            self.sphere.update_gl_state(blend=False)
-            self.sphere.set_data(self.data, face_color=ids, **kwargs)
-            img = canvas.render((pos[0] - click_radius,
-                                 pos[1] - click_radius,
-                                 click_radius * 2 + 1,
-                                 click_radius * 2 + 1),
-                                bgcolor=(0, 0, 0, 0))
-            self.sphere.update_gl_state(blend=True)
-            self.sphere.set_data(self.data, face_color=colors, **kwargs)
-            idxs = img.ravel().view(np.uint32)
-            idx = idxs[len(idxs) // 2]
+    def detect_idx(self):
+        click_radius = self.size/2
+        self.sphere.update_gl_state(blend=False)
+        self.sphere.antialias = 0
+        self.sphere.set_data(self.data, face_color=ids, **kwargs)
+        self.img = canvas.render((pos_press[0] - click_radius,
+                             pos_press[1] - click_radius,
+                             click_radius * 2 + 1,
+                             click_radius * 2 + 1),
+                            bgcolor=(0, 0, 0, 0))
+        self.sphere.update_gl_state(blend=True)
+        self.sphere.set_data(self.data, face_color=colors, **kwargs)
+        idxs = self.img.ravel().view(np.uint32)
+        idx = idxs[len(idxs) // 2]
 
-            if idx == 0:
-                idxs, counts = np.unique(idxs, return_counts=True)
-                idxs = idxs[np.argsort(counts)]
-                idx = idxs[-1] or (len(idxs) > 1 and idxs[-2])
-            if idx > 0:
-                self.on_click(idx - 1, self.sphere)
-                print('data coordinates :', self.data[idx])
+        if idx == 0:
+            idxs, counts = np.unique(idxs, return_counts=True)
+            idxs = idxs[np.argsort(counts)]
+            idx = idxs[-1] or (len(idxs) > 1 and idxs[-2])
+        if idx > 0:
+            self.on_click(idx - 1, self.sphere)
+            print('data coordinates :', self.data[idx])
+
+    def on_mouse_press(self, event):
+        global pos_press
+        pos_press = canvas.transforms.canvas_transform.map(event.pos)
+        if event.button == 1:
+            self.detect_idx()
 
     def on_click(self, idx, markers):
         colors[idx] = (1, 1, 1, 1)
-        markers.set_data(self.data, face_color=colors, symbol='o', size=45, edge_color=None)
 
+        markers.set_data(self.data, face_color=colors, symbol='o', size=self.size, edge_width=0, edge_color=None)
     def on_mouse_move(self, event):
-        global x, y, z, cx, cy, cz
+        global x, y, z
         tr = canvas.scene.node_transform(view.scene)
         scene_coords = tr.map(event.pos)
         scene_coords /= scene_coords[-1]
@@ -103,4 +118,3 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = MyApp()
     app.exec_()
-
