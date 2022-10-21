1d97a08833c6b932094c55f138715a0c44ba641a
junhyeon Jo <colorful105@cu.ac.kr>
2022-10-21 19:02:28 +0900

    Enable file dialog function and solve scaling error

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import numpy as np
 import sys
 from PyQt5.QtWidgets import *
 import vispy.scene
+from vispy import scene
 from vispy.scene import visuals
 from vispy import color
-import os
+import open3d
 
 class MyApp(QWidget):
     def __init__(self):
@@ -21,9 +22,8 @@ class MyApp(QWidget):
 
         view.add(grid)
         view.camera = 'turntable'
-        self.roll = view.camera.roll
 
-        button = QPushButton('Button')
+        button = QPushButton('Open File')
         button.clicked.connect(self.openfile)
         vbox = QVBoxLayout()
         vbox.addWidget(canvas.native)
@@ -34,24 +34,20 @@ class MyApp(QWidget):
         view.camera.center = (0, 0, 0)
         self.show()
         self.meshes = []
-        object_coordinate = "Object X: 0.000000 Y: 0.000000 Z: 0.000000"
         xyztext = "X: 0.000000 Y: 0.000000 Z: 0.000000"
         self.text = visuals.Text(xyztext, color='white', anchor_x='left', parent=view, pos=(20, 30))
-        self.text2 = visuals.Text(object_coordinate, color='white', anchor_x='left', parent=view, pos=(330, 520))
         canvas.events.key_press.connect(self.on_button_press)
         canvas.events.mouse_move.connect(self.on_mouse_move)
         canvas.events.mouse_press.connect(self.on_mouse_press)
         canvas.events.mouse_move.disconnect(self.on_mouse_press)
-
     def openfile(self):
-        QFileDialog.getOpenFileName(self, 'Open file', './')
+        self.file=QFileDialog.getOpenFileName(self, 'Open file', './')
+        self.object = self.EditVisual()
+        view.camera.distance = abs(self.data).max() * 2.5
+        self.meshes.append([self.data])
+        self.update()
 
     def on_button_press(self, event):
-        if event.key == 'c': # create object
-            self.object = self.EditVisual()
-            view.camera.distance = abs(self.data).max() * 2.5
-            self.meshes.append([self.data])
-            self.update()
 
         if event.key == 'f': # camera scene fix
             view.camera.interactive = not view.camera.interactive
@@ -60,14 +56,15 @@ class MyApp(QWidget):
                 canvas.events.mouse_move.connect(self.on_mouse_press)
             else:
                 canvas.events.mouse_move.disconnect(self.on_mouse_press)
-
     def EditVisual(self):
         global ids, kwargs, colors
-        self.size = 30
-        self.data = np.random.randn(100, 3)
+        pcd = open3d.io.read_point_cloud(self.file[0])
+        self.size = 0.1
+        self.data = np.asarray(pcd.points)
         cmap = color.get_colormap('viridis')
         colors = np.concatenate(list(map(cmap.map, np.linspace(0, 1, len(self.data)))))
-        self.sphere = visuals.Markers(parent=view.scene)
+        self.sphere = visuals.Markers(scaling=True, parent=view.scene)
+        self.sphere.transform = scene.transforms.STTransform(scale=(1, 1, 1))
         kwargs = dict(symbol='o', size=self.size, edge_width = 0, edge_color=None)
         ids = np.arange(1, len(self.data) + 1, dtype=np.uint32).view(np.uint8)
         ids = ids.reshape(-1, 4)
@@ -97,8 +94,7 @@ class MyApp(QWidget):
             idx = idxs[-1] or (len(idxs) > 1 and idxs[-2])
         if idx > 0:
             self.on_click(idx - 1, self.sphere)
-            data_x, data_y, data_z = self.data[idx]
-            self.text2.text = f"Object X: {data_x:0.06f} Y: {data_y:0.06f} Z: {data_z:0.06f}"
+            print('data coordinates :', self.data[idx])
 
     def on_mouse_press(self, event):
         global pos_press
@@ -117,7 +113,6 @@ class MyApp(QWidget):
         scene_coords /= scene_coords[-1]
         x, y, z, w = scene_coords
         self.text.text = f"X: {x:0.06f} Y: {y:0.06f} Z: {z:0.06f}"
-
         self.update()
 
 if __name__ == '__main__':
